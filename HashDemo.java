2e02112d9a7be30a9e2f7a918f2e373ddf7c2d3c
jiucheng <280695680@qq.com>
2021-06-09 17:41:17 +0800

    454. 四数相加 II

--- HashDemo.java
+++ HashDemo.java
@@ -20,6 +20,52 @@ public class HashDemo {
 
     }
 
+
+    /**
+     * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+     * <p>
+     * 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，
+     * 且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 2^31 - 1 。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/4sum-ii
+     * <p>
+     * 输入:
+     * A = [ 1, 2]
+     * B = [-2,-1]
+     * C = [-1, 2]
+     * D = [ 0, 2]
+     * <p>
+     * 输出:
+     * 2
+     * <p>
+     * 解释:
+     * 两个元组如下:
+     * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+     * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+     * <p>
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int ansCount = 0;
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int first : nums1) {
+            for (int second : nums2) {
+                countAB.merge(first + second, 1, Integer::sum);
+//                countAB.put(first + second, countAB.getOrDefault(first + second, 0) + 1);
+            }
+        }
+        for (int third : nums3) {
+            for (int forth : nums4) {
+                int sum = third + forth;
+                if (countAB.get(-sum) != null) {
+                    ansCount += countAB.get(-sum);
+                }
+            }
+        }
+
+        return ansCount;
+    }
+
+
     /**
      * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，
      * 判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
