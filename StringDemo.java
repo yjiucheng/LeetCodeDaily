f66a527bf76e628e6891c1bcea65f3e9b26e5fc4
jiucheng <280695680@qq.com>
2021-06-09 17:58:48 +0800

    344. 反转字符串

--- StringDemo.java
+++ StringDemo.java
@@ -4,7 +4,24 @@ import java.util.Stack;
 
 public class StringDemo {
     public static void main(String[] args) {
+        System.err.println(5 / 2);
+    }
 
+    /**
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * <p>
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * <p>
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/reverse-string
+     */
+    public void reverseString(char[] s) {
+        for (int index = 0; index < s.length / 2; index++) {
+            char temp = s[index];
+            s[index] = s[s.length - 1 - index];
+            s[s.length - 1 - index] = temp;
+        }
     }
 
 
