b25f9854d609473dd87102bb4b3d44558eff7640
jiucheng <280695680@qq.com>
2021-06-09 11:11:07 +0800

    202. 快乐数

--- HashDemo.java
+++ HashDemo.java
@@ -10,11 +10,63 @@ public class HashDemo {
         int[] s1 = new int[]{1, 2, 2, 1};
         int[] s2 = new int[]{2, 2};
 
-        System.err.println(Arrays.toString(intersect(s1, s2)));
+//        System.err.println(Arrays.toString(intersect(s1, s2)));
 
+        int s = 3;
+
+        System.err.println(isHappy(19));
 
         System.err.println("本次耗时：" + (System.currentTimeMillis() - starttime));
 
+    }
+
+
+    /**
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     * <p>
+     * 「快乐数」定义为：
+     * <p>
+     * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+     * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+     * 如果 可以变为  1，那么这个数就是快乐数。
+     * 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+     * <p>
+     * 1 <= n <= 2^31 - 1
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/happy-number
+     * <p>
+     * 输入：19
+     * 输出：true
+     * 解释：
+     * 1^2 + 9^2 = 82
+     * 8^2 + 2^2 = 68
+     * 6^2 + 8^2 = 100
+     * 1^2 + 0^2 + 0^2 = 1
+     */
+    public static boolean isHappy(int n) {
+        int sum = 0;
+        Set<Integer> numSet = new HashSet<>();
+        while (true) {
+            int lastNum = n % 10;
+            sum += lastNum * lastNum;
+            n = n / 10;
+            while (n > 0) {
+                lastNum = n % 10;
+                sum += lastNum * lastNum;
+                n = n / 10;
+            }
+            n = sum;
+            sum=0;
+            if (n == 1) {
+                return true;
+            } else {
+                if (numSet.contains(n)) {
+                    return false;
+                } else {
+                    numSet.add(n);
+                }
+            }
+        }
     }
 
 
