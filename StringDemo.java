015310eb57610996587e3d81e91824d2b478c86c
jiucheng <280695680@qq.com>
2021-06-10 17:45:17 +0800

    28. 实现 strStr()

--- StringDemo.java
+++ StringDemo.java
@@ -2,8 +2,49 @@ import java.util.*;
 
 public class StringDemo {
     public static void main(String[] args) {
-        System.err.println(reverseWords("  Bob    Loves  Alice   "));
+        System.err.println(strStr("mississippi", "issip"));
         String s = "123";
+    }
+
+
+    /**
+     * 实现 strStr() 函数。
+     * <p>
+     * 给你两个字符串 haystack 和 needle ，
+     * 请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+     * 说明：
+     * <p>
+     * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+     * <p>
+     * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+     * <p>
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/implement-strstr
+     * 输入：haystack = "hello", needle = "ll"
+     * 输出：2
+     * 输入：haystack = "aaaaa", needle = "bba"
+     * 输出：-1
+     * 输入：haystack = "", needle = ""
+     * 输出：0
+     */
+    public static int strStr(String haystack, String needle) {
+        if (needle == null || needle.length() == 0) {
+            return 0;
+        }
+        int ans = -1;
+        char[] haysChars = haystack.toCharArray();
+        char[] needleChars = needle.toCharArray();
+        for (int fast = 0; fast <= haysChars.length - needleChars.length; fast++) {
+            char cur = haysChars[fast];
+            if (cur == needleChars[0]) {
+                String str = haystack.substring(fast, fast + needleChars.length);
+                if (needle.equals(str)) {
+                    ans = fast;
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 
 
@@ -23,7 +64,7 @@ public class StringDemo {
     public String reverseLeftWords(String s, int n) {
         StringBuilder builder = new StringBuilder();
         builder.append(s.substring(n, s.length()));
-        builder.append(s.substring(0,n));
+        builder.append(s.substring(0, n));
 
         return builder.toString();
     }
