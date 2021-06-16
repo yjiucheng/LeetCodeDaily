3ff89a7368efb78e5d952efbe4b14c020372142b
jiucheng <280695680@qq.com>
2021-06-16 10:37:02 +0800

    1047. 删除字符串中的所有相邻重复项---优化

--- StackDemo.java
+++ StackDemo.java
@@ -7,7 +7,7 @@ import java.util.Stack;
  */
 public class StackDemo {
     public static void main(String[] args) {
-System.err.println(removeDuplicates("abbaca"));
+        System.err.println(removeDuplicates("abbaca"));
     }
 
     /**
@@ -17,23 +17,19 @@ System.err.println(removeDuplicates("abbaca"));
         if (s == null || s.length() <= 1) {
             return s;
         }
-        Stack<Character> stack = new Stack<>();
-        for (int i = s.length()-1; i >=0; i--) {
+        StringBuilder sb = new StringBuilder();
+        for (int i =0; i <s.length() ;i++) {
             char cur = s.charAt(i);
-            if (stack.size() > 0) {
-                char stackChar = stack.peek();
+            if (sb.length() > 0) {
+                char stackChar = sb.charAt(sb.length() - 1);
                 if (cur == stackChar) {
-                    stack.pop();
+                    sb.deleteCharAt(sb.length() - 1);
                 } else {
-                    stack.push(cur);
+                    sb.append(cur);
                 }
             } else {
-                stack.push(cur);
+                sb.append(cur);
             }
-        }
-        StringBuilder sb = new StringBuilder();
-        while (!stack.empty()) {
-            sb.append(stack.pop());
         }
         return sb.toString();
     }
