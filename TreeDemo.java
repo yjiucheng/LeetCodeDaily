e676ca97b4ea30298c86e53400d66975d7d56563
jiucheng <280695680@qq.com>
2021-06-21 11:40:26 +0800

    94. 二叉树的中序遍历--二叉树的递归中序遍历

--- TreeDemo.java
+++ TreeDemo.java
@@ -42,4 +42,21 @@ public class TreeDemo {
         afterOrder(root.right, ans);
         ans.add(root.val);
     }
+
+    /**
+     * 94. 二叉树的中序遍历
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+    public void inorder(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
 }
