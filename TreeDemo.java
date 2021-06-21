3e93524d9e2e87728bf5ec29dda235dd3e88ce7c
jiucheng <280695680@qq.com>
2021-06-21 11:33:38 +0800

    144. 二叉树的前序遍历--二叉树的递归前序遍历

---
+++ TreeDemo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeDemo {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 144. 二叉树的前序遍历
+     * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root, ans);
+        return ans;
+    }
+
+    public void preOrder(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        ans.add(root.val);
+        preOrder(root.left, ans);
+        preOrder(root.right, ans);
+    }
+}
