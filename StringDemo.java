c74dbc1795be0d8babd0589986ffe45320f998e2
jiucheng <280695680@qq.com>
2021-06-27 22:35:10 +0800

    70. 爬楼梯

--- StringDemo.java
+++ StringDemo.java
@@ -3,9 +3,36 @@ import java.util.*;
 public class StringDemo {
     public static void main(String[] args) {
 //        System.err.println(strStr("mississippi", "issip"));
-        String[] nums = new String[]{"flower","flow","flight"};
+        String[] nums = new String[]{"flower", "flow", "flight"};
         System.err.println(longestCommonPrefix(nums));
         String s = "123";
+    }
+
+    /**
+     * 70. 爬楼梯
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * <p>
+     * 注意：给定 n 是一个正整数。
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int p = 0, q = 1, r = 2;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
     }
 
 
