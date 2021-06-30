9f33fd10c7100abdf7f1df690c798b2475787635
jiucheng <280695680@qq.com>
2021-06-30 11:13:26 +0800

    429. N 叉树的层序遍历---BFS解法

--- TreeDemo.java
+++ TreeDemo.java
@@ -7,49 +7,99 @@ public class TreeDemo {
     }
 
     /**
-     * 637. 二叉树的层平均值
-     * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+     * 429. N 叉树的层序遍历
+     * 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+     * <p>
+     * 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+     * 输入：root = [1,null,3,2,4,null,5,6]
+     * 输出：[[1],[3,2,4],[5,6]]
      */
-    public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> ans = new ArrayList<>();
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
         if (root == null) {
             return ans;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
         queue.add(root);
         while (queue.size() > 0) {
             int count = queue.size();
-            double sum = 0;
+            List<Integer> list = new ArrayList<>();
             for (int i = 0; i < count; i++) {
-                TreeNode node = queue.remove();
-                sum += node.val;
-
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
+                Node node = queue.poll();
+                list.add(node.val);
+                List<Node> nodes = node.children;
+                if (nodes != null) {
+                    for (Node item : nodes) {
+                        queue.add(item);
+                    }
                 }
             }
-            ans.add(sum / count);
+            ans.add(list);
         }
-
         return ans;
-
     }
 
+    public static class Node {
+        public int val;
+        public List<Node> children;
 
-    /**
-     * 199. 二叉树的右视图
-     * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
-     */
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if (root == null) {
-            return ans;
+        public Node() {
         }
-        //DFS解法
-        getRightVal(root, 0, ans);
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+
+
+        /**
+         * 637. 二叉树的层平均值
+         * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+         */
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Double> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (queue.size() > 0) {
+                int count = queue.size();
+                double sum = 0;
+                for (int i = 0; i < count; i++) {
+                    TreeNode node = queue.remove();
+                    sum += node.val;
+
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+                ans.add(sum / count);
+            }
+
+            return ans;
+
+        }
+
+
+        /**
+         * 199. 二叉树的右视图
+         * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+         */
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            //DFS解法
+            getRightVal(root, 0, ans);
 //        BFS解法
 //        Queue<TreeNode> queue = new LinkedList<>();
 //        queue.add(root);
@@ -68,209 +118,210 @@ public class TreeDemo {
 //                }
 //            }
 //        }
-        return ans;
-    }
-
-
-    private void getRightVal(TreeNode node, int depth, List<Integer> list) {
-        if (node == null) {
-            return;
-        }
-        if (depth == list.size()) {
-            list.add(node.val);
-        }
-        depth++;
-        getRightVal(node.right, depth, list);
-        getRightVal(node.left, depth, list);
-    }
-
-    /**
-     * 107. 二叉树的层序遍历 II
-     * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
-     */
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
             return ans;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (queue.size() > 0) {
-            int count = queue.size();
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                TreeNode node = queue.poll();
+
+
+        private void getRightVal(TreeNode node, int depth, List<Integer> list) {
+            if (node == null) {
+                return;
+            }
+            if (depth == list.size()) {
                 list.add(node.val);
+            }
+            depth++;
+            getRightVal(node.right, depth, list);
+            getRightVal(node.left, depth, list);
+        }
+
+        /**
+         * 107. 二叉树的层序遍历 II
+         * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+         */
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (queue.size() > 0) {
+                int count = queue.size();
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < count; i++) {
+                    TreeNode node = queue.poll();
+                    list.add(node.val);
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+
+                }
+                ans.add(0, list);
+            }
+            return ans;
+        }
+
+
+        /**
+         * 102. 二叉树的层序遍历
+         * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+         *
+         * @param root
+         * @return
+         */
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (queue.size() > 0) {
+                int count = queue.size();
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < count; i++) {
+                    TreeNode node = queue.poll();
+                    list.add(node.val);
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+                ans.add(list);
+            }
+            return ans;
+        }
+
+        private void getLevelQueue(TreeNode treeNode, Queue<TreeNode> queue) {
+
+        }
+
+        /**
+         * 100. 相同的树
+         * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+         * <p>
+         * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+         */
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            boolean ans = true;
+
+            return ans;
+        }
+
+
+        /**
+         * 144. 二叉树的前序遍历
+         * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+         */
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+            while (stack.size() > 0) {
+                TreeNode node = stack.pop();
+                ans.add(node.val);
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
                 if (node.left != null) {
-                    queue.add(node.left);
+                    stack.push(node.left);
+                }
+            }
+
+            return ans;
+        }
+
+        public void preOrder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+            ans.add(root.val);
+            preOrder(root.left, ans);
+            preOrder(root.right, ans);
+        }
+
+        /**
+         * 145. 二叉树的后序遍历
+         * 给定一个二叉树，返回它的 后序 遍历。
+         */
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            Stack<TreeNode> stackOri = new Stack<>();
+            Stack<TreeNode> stackRes = new Stack<>();
+            stackOri.push(root);
+            while (!stackOri.isEmpty()) {
+                TreeNode node = stackOri.pop();
+                stackRes.push(node);
+
+                if (node.left != null) {
+                    stackOri.push(node.left);
                 }
                 if (node.right != null) {
-                    queue.add(node.right);
+                    stackOri.push(node.right);
                 }
-
             }
-            ans.add(0, list);
-        }
-        return ans;
-    }
+            while (!stackRes.isEmpty()) {
+                ans.add(stackRes.pop().val);
+            }
 
 
-    /**
-     * 102. 二叉树的层序遍历
-     * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
-     *
-     * @param root
-     * @return
-     */
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
             return ans;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (queue.size() > 0) {
-            int count = queue.size();
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                TreeNode node = queue.poll();
-                list.add(node.val);
-                if (node.left != null) {
-                    queue.add(node.left);
+
+        public void afterOrder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+            afterOrder(root.left, ans);
+            afterOrder(root.right, ans);
+            ans.add(root.val);
+        }
+
+        /**
+         * 94. 二叉树的中序遍历
+         */
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            if (root == null) {
+                return ans;
+            }
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while (!stack.isEmpty() || cur != null) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
                 }
+                TreeNode node = stack.pop();
+                ans.add(node.val);
                 if (node.right != null) {
-                    queue.add(node.right);
+                    cur = node.right;
                 }
             }
-            ans.add(list);
-        }
-        return ans;
-    }
-
-    private void getLevelQueue(TreeNode treeNode, Queue<TreeNode> queue) {
-
-    }
-
-    /**
-     * 100. 相同的树
-     * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
-     * <p>
-     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
-     */
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean ans = true;
-
-        return ans;
-    }
 
 
-    /**
-     * 144. 二叉树的前序遍历
-     * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
-     */
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if (root == null) {
             return ans;
         }
 
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while (stack.size() > 0) {
-            TreeNode node = stack.pop();
-            ans.add(node.val);
-            if (node.right != null) {
-                stack.push(node.right);
+        public void inorder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
             }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
+            inorder(root.left, ans);
+            ans.add(root.val);
+            inorder(root.right, ans);
         }
-
-        return ans;
-    }
-
-    public void preOrder(TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
-        }
-        ans.add(root.val);
-        preOrder(root.left, ans);
-        preOrder(root.right, ans);
-    }
-
-    /**
-     * 145. 二叉树的后序遍历
-     * 给定一个二叉树，返回它的 后序 遍历。
-     */
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if (root == null) {
-            return ans;
-        }
-        Stack<TreeNode> stackOri = new Stack<>();
-        Stack<TreeNode> stackRes = new Stack<>();
-        stackOri.push(root);
-        while (!stackOri.isEmpty()) {
-            TreeNode node = stackOri.pop();
-            stackRes.push(node);
-
-            if (node.left != null) {
-                stackOri.push(node.left);
-            }
-            if (node.right != null) {
-                stackOri.push(node.right);
-            }
-        }
-        while (!stackRes.isEmpty()) {
-            ans.add(stackRes.pop().val);
-        }
-
-
-        return ans;
-    }
-
-    public void afterOrder(TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
-        }
-        afterOrder(root.left, ans);
-        afterOrder(root.right, ans);
-        ans.add(root.val);
-    }
-
-    /**
-     * 94. 二叉树的中序遍历
-     */
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if (root == null) {
-            return ans;
-        }
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
-        while (!stack.isEmpty() || cur != null) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
-            TreeNode node = stack.pop();
-            ans.add(node.val);
-            if (node.right != null) {
-                cur = node.right;
-            }
-        }
-
-
-        return ans;
-    }
-
-    public void inorder(TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
-        }
-        inorder(root.left, ans);
-        ans.add(root.val);
-        inorder(root.right, ans);
     }
 }
