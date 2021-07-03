ccc818f7968e7035453f7f448a38d01bc3f3933c
jiucheng <280695680@qq.com>
2021-07-03 22:42:33 +0800

    104. 二叉树的最大深度

--- TreeDemo.java
+++ TreeDemo.java
@@ -9,6 +9,37 @@ public class TreeDemo {
     }
 
     /**
+     * 104. 二叉树的最大深度
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (queue.size() > 0) {
+            int count = queue.size();
+            if (count > 0) {
+                depth++;
+            }
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+    /**
      * 101. 对称二叉树
      *
      * @param root
