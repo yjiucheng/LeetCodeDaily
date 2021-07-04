a4df641ed76f8b711c47b23135e9fe09ec51f531
Null <280695680@qq.com>
2021-07-04 21:54:05 +0800

    645. 错误的集合

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -15,6 +15,33 @@ public class ArrayDemo {
         System.err.println(subarraySum(nums, 3));
     }
 
+   /**
+     * 645. 错误的集合
+     集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
+
+给定一个数组 nums 代表了集合 S 发生错误后的结果。
+
+请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+
+     */
+    public int[] findErrorNums(int[] nums) {
+         int[] newData=new int[nums.length+1];
+         for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            newData[num]+=1; 
+         }
+         int[] ans=new int[2];
+         for(int i=1;i<newData.length;i++){
+             if(newData[i]==2){
+                ans[0]=    i;
+             }
+             if(newData[i]==0){
+                 ans[1]=i;
+             }
+         }    
+         return ans;
+    }
+    
 
     /**
      * 560. 和为K的子数组
