762b500b892d5f502c0cea8f208a46cdf0bc6e6d
jiucheng <280695680@qq.com>
2021-07-17 23:39:54 +0800

    136. 只出现一次的数字--位运算

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -21,19 +21,11 @@ public class ArrayDemo {
      * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
      */
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length==1){
-            return nums[0];
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
         }
-        if(nums[1]!=nums[0]){
-            return nums[0];
-        }
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
-        }
-        return nums[nums.length-1];
+        return ans;
     }
 
     /**
