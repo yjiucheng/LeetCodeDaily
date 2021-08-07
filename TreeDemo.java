bb2c9ffee6e755ced2f2d08e2cdbe1ad82701cb9
jiucheng <280695680@qq.com>
2021-08-07 22:34:01 +0800

    543. 二叉树的直径

--- TreeDemo.java
+++ TreeDemo.java
@@ -8,6 +8,46 @@ public class TreeDemo {
         System.err.println(buildTree(preorder, inorder));
     }
 
+
+    /**
+     * 543. 二叉树的直径
+     * 给定一棵二叉树，你需要计算它的直径长度。
+     * 一棵二叉树的直径长度是任意两个结点路径长度中的最大值。
+     * 这条路径可能穿过也可能不穿过根结点。
+     */
+    int maxLength = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        maxLength = Math.max(maxLength, leftDepth + rightDepth + 1);
+
+        return Math.max(leftDepth, rightDepth) + 1; // 返回该节点为根的子树的深度
+    }
+
+
+    /**
+     * 236. 二叉树的最近公共祖先
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+
     /**
      * 501. 二叉搜索树中的众数
      *
