455408dae25d2f2b8a178e97bdec302370827717
jiucheng <280695680@qq.com>
2021-08-09 17:22:59 +0800

    1137. 第 N 个泰波那契数

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -19,19 +19,31 @@ public class ArrayDemo {
 
     /**
      * 1137. 第 N 个泰波那契数
+     *
      * @param n
      * @return
      */
     public int tribonacci(int n) {
-        if (n == 0) {
-            return 0;
+//        if (n == 0) {
+////            return 0;
+////        }
+////        if (n == 1 || n == 2) {
+////            return 1;
+////        }
+////        if (cache[n] != 0) return cache[n];
+////        cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+////        return cache[n];
+
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
         }
-        if (n == 1 || n == 2) {
-            return 1;
-        }
-        if (cache[n] != 0) return cache[n];
-        cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
-        return cache[n];
+        return c;
     }
 
     /**
