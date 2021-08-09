6f5c8310849895f032ca9f5d37692f62749f9057
jiucheng <280695680@qq.com>
2021-08-09 17:21:20 +0800

    1137. 第 N 个泰波那契数

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -15,14 +15,24 @@ public class ArrayDemo {
         System.err.println(subarraySum(nums, 3));
     }
 
+    int[] cache = new int[40];
+
+    /**
+     * 1137. 第 N 个泰波那契数
+     * @param n
+     * @return
+     */
     public int tribonacci(int n) {
         if (n == 0) {
             return 0;
         }
-        if(n===1){
-
-
-        }    }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        if (cache[n] != 0) return cache[n];
+        cache[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+        return cache[n];
+    }
 
     /**
      * 1337. 矩阵中战斗力最弱的 K 行
