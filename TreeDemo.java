a90f1ab1f4536c6bc4ebbcaba78488cfea3ba5fd
jiucheng <280695680@qq.com>
2021-08-17 11:33:51 +0800

    701. 二叉搜索树中的插入操作

--- TreeDemo.java
+++ TreeDemo.java
@@ -15,6 +15,42 @@ public class TreeDemo {
 
 
     /**
+     * 701. 二叉搜索树中的插入操作
+     * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。
+     * 返回插入后二叉搜索树的根节点。
+     * 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+     * <p>
+     * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+     */
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode ans = root;
+        insertIntoBST2(root, val);
+        return ans;
+    }
+
+    private void insertIntoBST2(TreeNode root, int val) {
+        if (root.val > val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+                return;
+            } else {
+                insertIntoBST2(root.left, val);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+                return;
+            } else {
+                insertIntoBST2(root.right, val);
+            }
+        }
+    }
+
+
+    /**
      * 235. 二叉搜索树的最近公共祖先
      *
      * @param root
@@ -30,7 +66,7 @@ public class TreeDemo {
             return lowestCommonAncestor3(root.right, p, q);
         } else if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor3(root.left, p, q);
-        }else {
+        } else {
             return root;
         }
     }
