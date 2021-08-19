430d7160d1395dd9da00bc702f282690623f2f0a
jiucheng <280695680@qq.com>
2021-08-19 11:43:31 +0800

    669. 修剪二叉搜索树

--- TreeDemo.java
+++ TreeDemo.java
@@ -24,6 +24,25 @@ public class TreeDemo {
 
 
     /**
+     * 669. 修剪二叉搜索树
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > high) {
+            root = trimBST(root.left, low, high);
+        } else if (root.val < low) {
+            root = trimBST(root.right, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        return root;
+    }
+
+
+    /**
      * 450. 删除二叉搜索树中的节点
      */
     public static TreeNode deleteNode(TreeNode root, int key) {
@@ -52,7 +71,7 @@ public class TreeDemo {
         } else if (root.val < key) {
             root.right = deleteNode2(root.right, key);
         } else {
-            root.left = deleteNode2(root.left, key );
+            root.left = deleteNode2(root.left, key);
         }
         return root;
     }
