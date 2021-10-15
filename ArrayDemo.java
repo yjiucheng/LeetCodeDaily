ed36775d665d937d690de3b900392cecb509b0c0
jiucheng <280695680@qq.com>
2021-10-15 16:50:14 +0800

    217. 存在重复元素

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 数组相关
@@ -18,13 +21,19 @@ public class ArrayDemo {
     /**
      * 217. 存在重复元素
      *
-     * @给定一个整数数组，判断是否存在重复元素。
-     * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+     * @给定一个整数数组，判断是否存在重复元素。 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
      */
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if(!set.add(num)){
+//        Set<Integer> set = new HashSet<>();
+//        for (int num : nums) {
+//            if(!set.add(num)){
+//                return true;
+//            }
+//        }
+//        return false;
+        Arrays.sort(nums);
+        for (int i=1;i<nums.length;i++){
+            if (nums[i-1]==nums[i]){
                 return true;
             }
         }
