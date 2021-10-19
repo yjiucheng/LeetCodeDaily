90a72f1fa63d651d6c5d7289bac156d176e478c4
jiucheng <280695680@qq.com>
2021-10-19 16:43:27 +0800

    216. 组合总和 III

--- BackTrackingDemo.java
+++ BackTrackingDemo.java
@@ -7,8 +7,54 @@ import java.util.List;
 public class BackTrackingDemo {
 
     public static void main(String[] args) {
-        System.err.println(combine(1, 1));
+//        System.err.println(combine(1, 1));
+        System.err.println(combinationSum3(3, 9));
     }
+
+
+    /**
+     * 216. 组合总和 III
+     * 找出所有相中加之和为 n 的 k 个数的组合。组合只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+     * <p>
+     * 说明：
+     * <p>
+     * 所有数字都是正整数。
+     * 解集不能包含重复的组合。 
+     * <p>
+     * 输入: k = 3, n = 7
+     * 输出: [[1,2,4]]
+     * <p>
+     * 输入: k = 3, n = 9
+     * 输出: [[1,2,6], [1,3,5], [2,3,4]]
+     */
+    static List<List<Integer>> ans216 = new ArrayList<>();
+
+    public static List<List<Integer>> combinationSum3(int k, int n) {
+//        for (int i = 1; i <= n - k; i++) {
+        combinationSum3(k, n, 1, 0, new ArrayList<>());
+//        }
+        return ans216;
+    }
+
+    public static void combinationSum3(int targetCount, int targetSum, int startIndex, int lastSum, List<Integer> temp) {
+        for (int i = startIndex; i <= targetSum - lastSum && i <= 9; i++) {
+            if (lastSum + i < targetSum) {
+                if (temp.size() < targetCount - 1) {
+                    temp.add(i);
+                    combinationSum3(targetCount, targetSum, i + 1, lastSum + i, temp);
+                    temp.remove(temp.size() - 1);
+                }
+            } else {
+                if (temp.size() == targetCount - 1) {
+                    temp.add(i);
+                    ans216.add(new ArrayList<>(temp));
+                    temp.remove(temp.size() - 1);
+                }
+                return;
+            }
+        }
+    }
+
 
     /**
      * 77. 组合
@@ -20,7 +66,7 @@ public class BackTrackingDemo {
     static List<List<Integer>> ans77 = new ArrayList<>();
 
     public static List<List<Integer>> combine(int n, int k) {
-        backtracking(n,k,1,new ArrayList<>());
+        backtracking(n, k, 1, new ArrayList<>());
         return ans77;
     }
 
