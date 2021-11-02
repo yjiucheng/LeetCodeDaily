72d197732b9b0d6e56295a9f15cebf5d18c9a3ee
jiucheng <280695680@qq.com>
2021-11-02 10:57:09 +0800

    56. 合并区间

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * 数组相关
@@ -16,6 +13,57 @@ public class ArrayDemo {
         System.err.println(Arrays.toString(nums));
 //        System.err.println(subarraySum(nums, 3));
     }
+
+
+    /**
+     * 56. 合并区间
+     * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]。
+     * 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+     * <p>
+     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * 输出：[[1,6],[8,10],[15,18]]
+     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     * <p>
+     */
+    public int[][] merge(int[][] intervals) {
+        int length = intervals.length * 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] nums = new int[length];
+        for (int i = 0; i < intervals.length; i++) {
+            nums[i * 2] = intervals[i][0];
+            nums[i * 2 + 1] = intervals[i][1];
+            map.put(intervals[i][0], Math.max(map.getOrDefault(intervals[i][0], Integer.MIN_VALUE), intervals[i][1]));
+        }
+        Arrays.sort(nums);
+        int index = 1;
+        int start = nums[0];
+        int maxEnd = map.get(start);
+        while (index < nums.length) {
+            int temp = nums[index];
+            if (temp <= maxEnd) {
+                if (map.get(temp) != null) {
+                    maxEnd = Math.max(map.get(temp), maxEnd);
+                }
+                map.remove(temp);
+                map.put(start, maxEnd);
+            } else {
+                start = temp;
+                maxEnd = map.get(start);
+            }
+            index++;
+        }
+        int[][] ans = new int[map.size()][2];
+        int ansIndex = 0;
+        for (int key : map.keySet()) {
+            int[] temp = new int[2];
+            temp[0] = key;
+            temp[1] = map.get(key);
+            ans[ansIndex] = temp;
+            ansIndex++;
+        }
+        return ans;
+    }
+
 
     /**
      * 217. 存在重复元素
