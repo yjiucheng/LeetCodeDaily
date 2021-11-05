367e2c41735e9cbee931e1ecbe21abb3636f4975
jiucheng <280695680@qq.com>
2021-11-05 15:39:51 +0800

    376. 摆动序列

--- GreedyAlgorithmDemo.java
+++ GreedyAlgorithmDemo.java
@@ -5,6 +5,34 @@ import java.util.Arrays;
  */
 public class GreedyAlgorithmDemo {
     public static void main(String[] args) {
+        int[] nums = new int[]{33, 53, 12, 64, 50, 41, 45, 21, 97, 35, 47, 92, 39, 0, 93, 55, 40, 46, 69, 42, 6, 95, 51, 68, 72, 9, 32, 84, 34, 64, 6, 2, 26, 98, 3, 43, 30, 60, 3, 68, 82, 9, 97, 19, 27, 98, 99, 4, 30, 96, 37, 9, 78, 43, 64, 4, 65, 30, 84, 90, 87, 64, 18, 50, 60, 1, 40, 32, 48, 50, 76, 100, 57, 29, 63, 53, 46, 57, 93, 98, 42, 80, 82, 9, 41, 55, 69, 84, 82, 79, 30, 79, 18, 97, 67, 23, 52, 38, 74, 15};
+        GreedyAlgorithmDemo demo = new GreedyAlgorithmDemo();
+        System.err.println(demo.wiggleMaxLength(nums));
+    }
+
+    /**
+     * 376. 摆动序列
+     * [33,53,12,64,50,41,45,21,97,35,47,92,39,0,93,55,40,46,69,42,6,95,51,68,72,9,32,84,34,64,6,2,26,98,3,43,30,60,3,68,82,9,97,19,27,98,99,4,30,96,37,9,78,43,64,4,65,30,84,90,87,64,18,50,60,1,40,32,48,50,76,100,57,29,63,53,46,57,93,98,42,80,82,9,41,55,69,84,82,79,30,79,18,97,67,23,52,38,74,15]
+     *
+     * @param nums
+     * @return
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int length = 1;
+
+        int lastDiffer = 0;
+        for (int index = 1; index < nums.length; index++) {
+            int curDiffer = nums[index] - nums[index - 1];
+
+            if ((curDiffer > 0 && lastDiffer <= 0) || (curDiffer < 0 && lastDiffer >= 0)) {
+                length++;
+                lastDiffer = curDiffer;
+            }
+        }
+        return length;
 
     }
 
