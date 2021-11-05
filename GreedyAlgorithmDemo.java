4f38904852a03361fa388670b9636b24d9f8f58c
jiucheng <280695680@qq.com>
2021-11-05 17:03:13 +0800

    122. 买卖股票的最佳时机 II

--- GreedyAlgorithmDemo.java
+++ GreedyAlgorithmDemo.java
@@ -6,9 +6,46 @@ import java.util.Arrays;
 public class GreedyAlgorithmDemo {
     public static void main(String[] args) {
 //        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
-        int[] nums = new int[]{-11, -14, -2};
+        int[] nums = new int[]{7,6,4,3,1};
         GreedyAlgorithmDemo demo = new GreedyAlgorithmDemo();
-        System.err.println(demo.maxSubArray(nums));
+        System.err.println(demo.maxProfit(nums));
+    }
+
+
+    /**
+     * 122. 买卖股票的最佳时机 II
+     * 给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+     * <p>
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     * <p>
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     */
+    public int maxProfit(int[] prices) {
+        //如果没有股票：今天价格比明天低，就买进来
+//          如果有股票：明天价格比今天低，就卖出去
+        boolean has = false;
+        int sum = 0;
+        int buyPrice = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (has) {
+                //有股票：明天价格比今天低，就卖出去
+                if (prices[i] > prices[i + 1]) {
+                    sum += prices[i] - buyPrice;
+                    has = false;
+                    buyPrice = 0;
+                }
+            } else {
+                //没有股票：今天价格比明天低，买进
+                if (prices[i] < prices[i + 1]) {
+                    buyPrice = prices[i];
+                    has = true;
+                }
+            }
+        }
+        if (has) {
+            sum += prices[prices.length - 1] - buyPrice;
+        }
+        return sum;
     }
 
 
