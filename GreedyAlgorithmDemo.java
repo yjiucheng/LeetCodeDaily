7804e09bafa4c41b9a85e1bfbddb5a222f96e288
jiucheng <280695680@qq.com>
2021-11-05 16:10:49 +0800

    53. 最大子序和

--- GreedyAlgorithmDemo.java
+++ GreedyAlgorithmDemo.java
@@ -5,10 +5,34 @@ import java.util.Arrays;
  */
 public class GreedyAlgorithmDemo {
     public static void main(String[] args) {
-        int[] nums = new int[]{33, 53, 12, 64, 50, 41, 45, 21, 97, 35, 47, 92, 39, 0, 93, 55, 40, 46, 69, 42, 6, 95, 51, 68, 72, 9, 32, 84, 34, 64, 6, 2, 26, 98, 3, 43, 30, 60, 3, 68, 82, 9, 97, 19, 27, 98, 99, 4, 30, 96, 37, 9, 78, 43, 64, 4, 65, 30, 84, 90, 87, 64, 18, 50, 60, 1, 40, 32, 48, 50, 76, 100, 57, 29, 63, 53, 46, 57, 93, 98, 42, 80, 82, 9, 41, 55, 69, 84, 82, 79, 30, 79, 18, 97, 67, 23, 52, 38, 74, 15};
+//        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = new int[]{-11,-14,-2};
         GreedyAlgorithmDemo demo = new GreedyAlgorithmDemo();
-        System.err.println(demo.wiggleMaxLength(nums));
+        System.err.println(demo.maxSubArray(nums));
     }
+
+    /**
+     * 53. 最大子序和
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int lastSum = nums[0] <= 0 ? 0 : nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            if (lastSum + num < 0) {
+                lastSum = 0;
+                max = Math.max(max, num);
+            } else {
+                max = Math.max(max, lastSum + num);
+                lastSum += num;
+            }
+        }
+        return max;
+    }
+
 
     /**
      * 376. 摆动序列
