9ed79b508575f0b941847f991c36ab9a4f26f88f
jiucheng <280695680@qq.com>
2021-11-06 15:16:05 +0800

    268. 丢失的数字

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -14,6 +14,22 @@ public class ArrayDemo {
 //        System.err.println(subarraySum(nums, 3));
     }
 
+    /**
+     * 268. 丢失的数字
+     * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+     */
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        for (int cur : nums) {
+            if (cur != start) {
+                return start;
+            }
+            start++;
+        }
+        return nums.length;
+    }
+
 
     /**
      * 56. 合并区间
