5076e72c7094f9c3a78b6884480f8976f9e6cb55
jiucheng <280695680@qq.com>
2021-11-10 18:00:27 +0800

    581. 最短无序连续子数组

--- GreedyAlgorithmDemo.java
+++ GreedyAlgorithmDemo.java
@@ -8,11 +8,42 @@ import java.util.List;
  */
 public class GreedyAlgorithmDemo {
     public static void main(String[] args) {
-        int[] nums = new int[]{5, 10, 20};
+        int[] nums = new int[]{1,3,2,2,2};
 //        int[] nums = new int[]{1, 2, 22, -23, -9, -30, -6, -9, 1, 8, 24, 2, 21, 29, 10, -25, 18, 30, 1, 9, -8, -11, -22, -23, -17, -12, 19, 28, 19, 28};
         String s = "AAAAAABCDEFG";
         GreedyAlgorithmDemo demo = new GreedyAlgorithmDemo();
-        System.err.println(demo.leastInterval(s.toCharArray(),2));
+        System.err.println(demo.findUnsortedSubarray(nums));
+    }
+
+
+    /**
+     * 581. 最短无序连续子数组
+     * 给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+     * <p>
+     * 请你找出符合题意的 最短 子数组，并输出它的长度。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+
+        for (int i = 1; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
     }
 
 
