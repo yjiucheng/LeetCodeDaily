4aa78ce24cf38765fa1749329300e32d8689aec8
jiucheng <280695680@qq.com>
2021-11-12 18:56:47 +0800

    63. 不同路径 II

--- DynamicProgrammingDemo.java
+++ DynamicProgrammingDemo.java
@@ -7,6 +7,48 @@ public class DynamicProgrammingDemo {
         System.err.println(demo.uniquePaths(5, 8));
     }
 
+
+    /**
+     * 63. 不同路径 II
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * <p>
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     * <p>
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] ans = new int[m][n];
+        ans[0][0] = 1 - obstacleGrid[0][0];
+
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 0 && ans[0][i - 1] == 1) {
+                ans[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0 && ans[i - 1][0] == 1) {
+                ans[i][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
+                }
+            }
+        }
+        return ans[m - 1][n - 1];
+    }
+
     /**
      * 62. 不同路径
      * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
@@ -20,7 +62,7 @@ public class DynamicProgrammingDemo {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public int uniquePaths(int m, int n) {
-        if (m == 1 || n==1) {
+        if (m == 1 || n == 1) {
             return 1;
         }
         int[][] ans = new int[m][n];
