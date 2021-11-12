c5d77ffd9823e399a58181076ea864db512e5e75
jiucheng <280695680@qq.com>
2021-11-12 18:35:40 +0800

    62. 不同路径

--- DynamicProgrammingDemo.java
+++ DynamicProgrammingDemo.java
@@ -3,6 +3,77 @@
  */
 public class DynamicProgrammingDemo {
     public static void main(String[] args) {
+        DynamicProgrammingDemo demo = new DynamicProgrammingDemo();
+        System.err.println(demo.uniquePaths(5, 8));
+    }
+
+    /**
+     * 62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * <p>
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * <p>
+     * 问总共有多少条不同的路径？
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-paths
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n==1) {
+            return 1;
+        }
+        int[][] ans = new int[m][n];
+        ans[0][0] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == 0) {
+                    ans[0][j] = 1;
+                }
+                if (j == 0) {
+                    ans[i][0] = 1;
+                }
+                if (i >= 1 && j >= 1) {
+                    ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
+                }
+            }
+        }
+
+        return ans[m - 1][n - 1];
+    }
+
+    /**
+     * 746. 使用最小花费爬楼梯x
+     * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+     * <p>
+     * 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+     * <p>
+     * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * <p>
+     * 输入：cost = [10, 15, 20]
+     * 输出：15
+     * 解释：最低花费是从 cost[1] 开始，然后走两步即可到阶梯顶 ，一共花费 15 。
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length <= 1) {
+            return 0;
+        }
+        int[] minPay = new int[cost.length];
+        minPay[0] = cost[0];
+        minPay[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            minPay[i] = Math.min(minPay[i - 1], minPay[i - 2]) + cost[i];
+        }
+
+
+        return Math.min(minPay[minPay.length - 1], minPay[minPay.length - 2]);
 
     }
 
